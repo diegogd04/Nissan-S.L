794b0791ea5aef1013bb9318b841535797e0f3f0
Unai-Gomez-Diez <unaigomez0807@gmail.com>
2023-01-24 19:39:11 +0100

    Creo la Interfaz Accesorios.java

---
+++ src/com/iesam/nissan/domain/models/Accesorios.java
@@ -0,0 +1,12 @@
+package com.iesam.nissan.domain.models;
+
+public interface Accesorios {
+    public String getCodigoAccesorio();
+    public String setCodigoAccesorio();
+
+    public String getModeloAccesorio();
+    public String setModeloAccesorio();
+
+    public String getUnidadesAccesorio();
+    public String setUnidadesAccesorio();
+}
